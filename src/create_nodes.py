44bc6ab2fc40f5bb4c16849f5275f81e7fe76a86
Nirvi1 <nirvir2@gmail.com>
2020-05-07 22:12:29 +0800

    Added NodeCreator

--- src/create_nodes.py
+++ src/create_nodes.py
@@ -1,7 +1,19 @@
 import csv
+class NodeCreator:
 
-class NodeCreator{
-    def __init__():
-        with open('data/sample.csv', 'r') as file:
-            recipe_data = file.read()
-}
+    def __init__(self, cleaned_data):
+        self.recipe_data = csv.reader(open(cleaned_data, mode = 'r', encoding = 'utf8'))
+
+    def generate_name_nodes(self):
+        recipe_nodes = []
+        with open('csvs/recipe_nodes.csv', 'w', newline = '') as f:
+            writer = csv.writer(f)
+            writer.writerows(['recipeId:ID(Recipe)', 'recipe', ':LABEL'])
+            
+        for row in self.recipe_data:
+            recipe_nodes.append([row[1], row[0]])
+            
+        with open('csvs/recipe_nodes.csv', 'a', newline='') as f:
+            writer = csv.writer(f)
+            writer.writerows(self.recipe_nodes)
+
